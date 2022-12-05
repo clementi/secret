0ce30ba9d00ba03ed2c41e154531fa21c7b553bd
Jeff Pratt <clementi@users.noreply.github.com>
2022-12-04 22:14:23 -0700

    Use include_str! instead of reading file

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -1,18 +1,12 @@
-use std::{
-    collections::HashMap,
-    error::Error,
-    fs::File,
-    io::{self, BufRead},
-};
+use std::{collections::HashMap, error::Error};
 
 pub fn load_dictionary() -> Result<HashMap<String, String>, Box<dyn Error>> {
-    let file = File::open("./eff_large_wordlist.txt")?;
-    let lines = io::BufReader::new(file).lines();
+    let dictionary_contents = include_str!("../eff_large_wordlist.txt");
+    let lines = dictionary_contents.lines();
 
     let mut dictionary = HashMap::new();
 
-    for result in lines {
-        let line = result.unwrap();
+    for line in lines {
         let parts: Vec<&str> = line.split('\t').collect();
         dictionary.insert(String::from(parts[0]), String::from(parts[1]));
     }
