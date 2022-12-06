fde85d4f26458f2b545eeeb20bba1f583be467b4
Jeff Pratt <jpratt@proofpoint.com>
2022-12-06 09:22:40 -0700

    Rename greater_than_zero -> parse_natural_number

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
 use clap::{Parser, Subcommand};
 
-fn greater_than_zero(v: &str) -> Result<u32, String> {
+fn parse_natural_number(v: &str) -> Result<u32, String> {
     let num: u32 = v.parse::<u32>().map_err(|_| String::from("--number must be an integer greater than zero"))?;
     if num > 0 {
         Ok(num)
@@ -21,7 +21,7 @@ pub struct Args {
         short = 'n',
         help = "Number of tokens or phrases to generate",
         default_value_t = 1,
-        value_parser = greater_than_zero
+        value_parser = parse_natural_number
     )]
     pub count: u32,
 }
@@ -29,7 +29,7 @@ pub struct Args {
 pub enum Command {
     #[command(about = "Generate a token (string) of random characters")]
     Token {
-        #[arg(long, short, help = "Length of token", default_value_t = 20, value_parser = greater_than_zero)]
+        #[arg(long, short, help = "Length of token", default_value_t = 20, value_parser = parse_natural_number)]
         length: u32,
 
         #[arg(long, help = "Use lowercase letters")]
@@ -65,10 +65,51 @@ pub enum Command {
     },
     #[command(about = "Generate a passphrase of random words")]
     Phrase {
-        #[arg(long, short, help = "Length of phrase (in words)", default_value_t = 4, value_parser = greater_than_zero)]
+        #[arg(long, short, help = "Length of phrase (in words)", default_value_t = 4, value_parser = parse_natural_number)]
         length: u32,
 
         #[arg(long, short, help = "Word separator", default_value_t = String::from(" "))]
         separator: String,
     },
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_natural_number_correctly_fails_on_non_numbers() {
+        let value = "q";
+        match parse_natural_number(value) {
+            Ok(v) => assert!(false, "should fail on {value}, got {v}"),
+            Err(_) => assert!(true),
+        }
+    }
+
+    #[test]
+    fn parse_natural_number_succeeds_on_positive_integers() {
+        let value = "1";
+        match parse_natural_number(value) {
+            Ok(v) => assert_eq!(v, 1),
+            Err(msg) => assert!(false, "{}", msg),
+        }
+    }
+
+    #[test]
+    fn parse_natural_number_fails_on_nonpositive_integers() {
+        let value = "0";
+        match parse_natural_number(value) {
+            Ok(v) => assert!(false, "should fail on {value}, got {v}"),
+            Err(_) => assert!(true),
+        }
+    }
+
+    #[test]
+    fn parse_natural_number_fails_on_nonintegral_numbers() {
+        let value = "1.1";
+        match parse_natural_number(value) {
+            Ok(v) => assert!(false, "should fail on {value}, got {v}"),
+            Err(_) => assert!(true),
+        }
+    }
 }
